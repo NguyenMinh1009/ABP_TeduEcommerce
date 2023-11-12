f8348e8660a1b1e23b9f7f73d263731d3eb005c1
Cloudy_Place <minhnnm1009@gmail.com>
2023-11-12 18:04:26 +0700

    Initial

---
+++ aspnet-core/src/TeduEcommerce.Domain/Promotions/PromotionManufacturer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeduEcommerce.Promotions
+{
+    public class PromotionManufacturer : Entity<Guid>
+    {
+        public Guid ManufactureId { get; set; }
+        public Guid PromotionId { get; set; }
+
+    }
+}
---
+++ aspnet-core/src/TeduEcommerce.Domain/Promotions/PromotionProduct.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeduEcommerce.Promotions
+{
+    public class PromotionProduct : Entity<Guid>
+    {
+        public Guid ProductId { get; set; }
+        public Guid PromotionId { get; set; }
+
+    }
+}
